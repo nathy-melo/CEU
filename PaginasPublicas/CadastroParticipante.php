f6bbbff3af22b1fc967fa227ddfe0e6bed72ff51
nathy-melo <natymelo2008@gmail.com>
2025-09-17 10:07:15 -0300

    nt

---
+++ PaginasPublicas/CadastroParticipante.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro de participante</title>
+</head>
+<body>
+    <?php
+        $conexao = mysqli_connect("localhost","root","","ceu_bd") 
+            or die ("Erro ao conectar." . mysqli_connect_error() );
+    $nome_completo = $_POST['nome_completo'];
+    $cpf = $_POST['cpf'];
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
+    
+    $sql = "INSERT INTO participante (CPF, Nome, Email, Senha) VALUES (
+        '$cpf', '$nome_completo', '$email', '$senha')";
+
+    mysqli_query($conexao, $sql) 
+        or die ("Erro ao tentar cadastrar registro." . mysqli_error($conexao));
+    ?>
+</body>
+</html>
